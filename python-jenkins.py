1f4abc2a84b7467b5187bbdaf80215a085772577
Mayankgoyal02 <33936226+Mayankgoyal02@users.noreply.github.com>
2023-12-21 20:10:55 +0530

    Update python-jenkins.py

--- python-jenkins.py
+++ python-jenkins.py
@@ -1,9 +1,9 @@
 import requests
 
-# Replace 'your_blazemeter_api_key' with your actual BlazeMeter API key
+
 blazemeter_api_key = 'dd24e562a1c804ac55446bbd'
 
-# BlazeMeter API endpoint for getting information about your account
+
 blazemeter_api_url = 'https://a.blazemeter.com/api/latest/accounts'
 
 # Set up the headers with the BlazeMeter API key
@@ -14,7 +14,6 @@ headers = {
 # Make a GET request to the BlazeMeter API with headers
 response = requests.get(blazemeter_api_url, headers=headers)
 
-# Check the response status
 if response.status_code == 200:
     # Print the response content (JSON data in this case)
     print(response.json())
