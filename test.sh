04b1ce77d19dbf0bb22590c8106d8960357d409f
Mayankgoyal02 <33936226+Mayankgoyal02@users.noreply.github.com>
2023-12-22 18:27:35 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+FILES_URL='https://a.blazemeter.com/api/v4/tests/13758845/files'
+USERNAME='ebf4a8d99d54eb292bcad9ce'
+API_KEY='8be234d747e4d099e67040d248764c3a9d747b1c6df4c5003b813b8403b5e6c6e511ae77'
 
+# Upload the JMX file
+curl -sk "$FILES_URL" \
+    -X POST \
+    -F "file=@performancetesting.jmx" \
+    --user "$USERNAME:$API_KEY"
+
+curl -sk "$FILES_URL" \
+    -X POST \
+    -F "file=@user.csv" \
+    --user "$USERNAME:$API_KEY"  
+
+curl -sk "$FILES_URL" \
+    -X POST \
+    -F "file=@data.csv" \
+    --user "$USERNAME:$API_KEY"      
