896db8309649c1c7964e536c4489fa3c4628a77c
Mayankgoyal02 <33936226+Mayankgoyal02@users.noreply.github.com>
2023-12-26 20:46:20 +0530

    copied lastest code fromblazemeter.sh

--- test.sh
+++ test.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
-FILES_URL='https://a.blazemeter.com/api/v4/tests/13758845/files'
+TEST_ID=13758845
+JMX_FILE='performancetesting.jmx'
+USER_CSV='user.csv'
+DATA_CSV='data.csv'
+FILES_URL="https://a.blazemeter.com/api/v4/tests/${TEST_ID}/files"
+RUN_TEST_URL="https://a.blazemeter.com/api/v4/tests/${TEST_ID}/start"
 USERNAME='ebf4a8d99d54eb292bcad9ce'
 API_KEY='8be234d747e4d099e67040d248764c3a9d747b1c6df4c5003b813b8403b5e6c6e511ae77'
 
 # Upload the JMX file
 curl -sk "$FILES_URL" \
     -X POST \
-    -F "file=@performancetesting.jmx" \
+    -F "file=@$JMX_FILE" \
     --user "$USERNAME:$API_KEY"
 
 curl -sk "$FILES_URL" \
     -X POST \
-    -F "file=@user.csv" \
+    -F "file=@$USER_CSV" \
     --user "$USERNAME:$API_KEY"  
 
 curl -sk "$FILES_URL" \
     -X POST \
-    -F "file=@data.csv" \
-    --user "$USERNAME:$API_KEY"      
+    -F "file=@$DATA_CSV" \
+    --user "$USERNAME:$API_KEY"     
+
+# curl -sk "$RUN_TEST_URL" \
+#     -X POST \
+#     -H 'Content-Type: application/json' \
+#     --user "$USERNAME:$API_KEY"    
