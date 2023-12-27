bc60f42bf9ff3ae70d86c0ab1ba8f87cdd4969be
Mayankgoyal02 <33936226+Mayankgoyal02@users.noreply.github.com>
2023-12-27 19:29:12 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,56 @@
+# #!/bin/bash
+ 
+# # Check if BLAZEMETER_TEST_ID is provided as an environment variable
+# if [ -z "${BLAZEMETER_TEST_ID}" ]; then
+#  echo "Error: BLAZEMETER_TEST_ID environment variable is not set."
+#  exit 1
+# fi
+ 
+# TEST_ID="${BLAZEMETER_TEST_ID}"
+ 
+# # Check if FILE_JMX, FILE_USER_CSV, and FILE_DATA_CSV are provided as environment variables
+# # if [ -z "${FILE_JMX}" ] || [ -z "${FILE_USER_CSV}" ] || [ -z "${FILE_DATA_CSV}" ]; then
+# #  echo "Error: FILE_JMX, FILE_USER_CSV, or FILE_DATA_CSV environment variables are not set."
+# #  exit 1
+# # fi
+ 
+# JMX_FILE="performancetesting.jmx"
+# USER_CSV="user.csv"
+# DATA_CSV="data.csv"
+ 
+# FILES_URL="https://a.blazemeter.com/api/v4/tests/${TEST_ID}/files"
+# RUN_TEST_URL="https://a.blazemeter.com/api/v4/tests/${TEST_ID}/start"
+# USERNAME='ebf4a8d99d54eb292bcad9ce'
+# API_KEY='8be234d747e4d099e67040d248764c3a9d747b1c6df4c5003b813b8403b5e6c6e511ae77'
+ 
+# # Upload the JMX file
+# curl -sk "$FILES_URL" \
+#  -X POST \
+#  -F "file=@$JMX_FILE" \
+#  --user "$USERNAME:$API_KEY"
+ 
+# # Upload the user CSV file
+# curl -sk "$FILES_URL" \
+#  -X POST \
+#  -F "file=@$USER_CSV" \
+#  --user "$USERNAME:$API_KEY"
+ 
+# # Upload the data CSV file
+# curl -sk "$FILES_URL" \
+#  -X POST \
+#  -F "file=@$DATA_CSV" \
+#  --user "$USERNAME:$API_KEY"
+ 
+# # Uncomment the following lines if you want to run the test immediately after uploading files
+# curl -sk "$RUN_TEST_URL" \
+# -X POST \
+# -H 'Content-Type: application/json' \
+# --user "$USERNAME:$API_KEY"
+
+# curl -sk "https://a.blazemeter.com/api/v4/masters?testId=${TEST_ID}" \
+#     --user "$USERNAME:$API_KEY"
+
+
 #!/bin/bash
  
 # Check if BLAZEMETER_TEST_ID is provided as an environment variable
@@ -41,11 +94,18 @@ curl -sk "$FILES_URL" \
  -F "file=@$DATA_CSV" \
  --user "$USERNAME:$API_KEY"
  
-# Uncomment the following lines if you want to run the test immediately after uploading files
-curl -sk "$RUN_TEST_URL" \
--X POST \
--H 'Content-Type: application/json' \
---user "$USERNAME:$API_KEY"
-
-curl -sk "https://a.blazemeter.com/api/v4/masters?testId=${TEST_ID}" \
-    --user "$USERNAME:$API_KEY"
+# Run the test and capture the response
+response=$(curl -sk "$RUN_TEST_URL" \
+ -X POST \
+ -H 'Content-Type: application/json' \
+ --user "$USERNAME:$API_KEY")
+ 
+# Extract the test ID from the response
+test_id=$(echo "$response" | jq -r '.id')
+ 
+if [ -n "$test_id" ]; then
+ # Print the BlazeMeter test dashboard URL
+ echo "BlazeMeter test dashboard URL: https://a.blazemeter.com/app/tests/$test_id"
+else
+ echo "Error: Failed to get test ID from BlazeMeter response."
+fi
