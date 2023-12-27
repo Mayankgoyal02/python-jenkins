9f4622f5a8f9154cf07d803e5c2918ba1d6b7a86
Mayankgoyal02 <33936226+Mayankgoyal02@users.noreply.github.com>
2023-12-27 19:38:13 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -61,15 +61,15 @@ fi
  
 TEST_ID="${BLAZEMETER_TEST_ID}"
  
-# Check if FILE_JMX, FILE_USER_CSV, and FILE_DATA_CSV are provided as environment variables
+# # Check if FILE_JMX, FILE_USER_CSV, and FILE_DATA_CSV are provided as environment variables
 # if [ -z "${FILE_JMX}" ] || [ -z "${FILE_USER_CSV}" ] || [ -z "${FILE_DATA_CSV}" ]; then
 #  echo "Error: FILE_JMX, FILE_USER_CSV, or FILE_DATA_CSV environment variables are not set."
 #  exit 1
 # fi
  
-JMX_FILE="performancetesting.jmx"
-USER_CSV="user.csv"
-DATA_CSV="data.csv"
+JMX_FILE="${performancetesting.jmx}"
+USER_CSV="${user.csv}"
+DATA_CSV="${data.csv}"
  
 FILES_URL="https://a.blazemeter.com/api/v4/tests/${TEST_ID}/files"
 RUN_TEST_URL="https://a.blazemeter.com/api/v4/tests/${TEST_ID}/start"
@@ -77,22 +77,22 @@ USERNAME='ebf4a8d99d54eb292bcad9ce'
 API_KEY='8be234d747e4d099e67040d248764c3a9d747b1c6df4c5003b813b8403b5e6c6e511ae77'
  
 # Upload the JMX file
-curl -sk "$FILES_URL" \
+response=$(curl -sk "$FILES_URL" \
  -X POST \
  -F "file=@$JMX_FILE" \
- --user "$USERNAME:$API_KEY"
+ --user "$USERNAME:$API_KEY")
  
 # Upload the user CSV file
-curl -sk "$FILES_URL" \
+response=$(curl -sk "$FILES_URL" \
  -X POST \
  -F "file=@$USER_CSV" \
- --user "$USERNAME:$API_KEY"
+ --user "$USERNAME:$API_KEY")
  
 # Upload the data CSV file
-curl -sk "$FILES_URL" \
+response=$(curl -sk "$FILES_URL" \
  -X POST \
  -F "file=@$DATA_CSV" \
- --user "$USERNAME:$API_KEY"
+ --user "$USERNAME:$API_KEY")
  
 # Run the test and capture the response
 response=$(curl -sk "$RUN_TEST_URL" \
@@ -100,8 +100,8 @@ response=$(curl -sk "$RUN_TEST_URL" \
  -H 'Content-Type: application/json' \
  --user "$USERNAME:$API_KEY")
  
-# Extract the test ID from the response
-test_id=$(echo "$response" | jq -r '.id')
+# Extract the test ID from the response using grep and awk
+test_id=$(echo "$response" | grep -o '"id":[^,]*' | awk -F':' '{print $2}' | tr -d '"')
  
 if [ -n "$test_id" ]; then
  # Print the BlazeMeter test dashboard URL
