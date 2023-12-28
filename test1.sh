76e05a10f33ea182f7aca7b9607072de7bbe9f69
Mayankgoyal02 <33936226+Mayankgoyal02@users.noreply.github.com>
2023-12-28 20:41:56 +0530

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,14 +1,62 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Check if BRANCH_NAME is provided by Jenkins
-if [ -z "${Branch}" ]; then
-  echo "BRANCH not provided by Jenkins. Please select a branch."
+# # Check if BRANCH_NAME is provided by Jenkins
+# if [ -z "${Branch}" ]; then
+#   echo "BRANCH not provided by Jenkins. Please select a branch."
+#   exit 1
+# fi
+ 
+# BRANCH="$Branch"
+# TEST_ID='13758845'
+# echo $Branch
+ 
+# # GitHub repository details
+# GITHUB_REPO="https://github.com/Mayankgoyal02/python-jenkins"
+# RAW_BASE_URL="https://raw.githubusercontent.com/Mayankgoyal02/python-jenkins/${BRANCH}/"
+ 
+# # BlazeMeter API details
+# FILES_URL="https://a.blazemeter.com/api/v4/tests/${TEST_ID}/files"
+# RUN_TEST_URL="https://a.blazemeter.com/api/v4/tests/${TEST_ID}/start"
+# USERNAME='ebf4a8d99d54eb292bcad9ce'
+# API_KEY='8be234d747e4d099e67040d248764c3a9d747b1c6df4c5003b813b8403b5e6c6e511ae77'
+ 
+# # Fetch the list of files with .jmx and .csv extensions from GitHub
+# file_list=$(curl -s "${GITHUB_REPO}/tree/${BRANCH}" | grep -Eo 'href="([^"#]+\.jmx|[^"#]+\.csv)"' | cut -d'"' -f2)
+ 
+# # Display the list of files (optional)
+# echo "Files to be uploaded:"
+# echo "$file_list"
+ 
+# # Upload files to BlazeMeter
+# for file in $file_list; do
+#   file_url="$RAW_BASE_URL$file"
+#   filename=$(basename "$file")
+ 
+#   # Upload each file individually using the provided curl command structure
+#   upload_response=$(curl -sk "$FILES_URL" \
+#     -X POST \
+#     -F "file=@$filename" \
+#     --user "$USERNAME:$API_KEY"
+#   )
+# done
+ 
+# Uncomment the following lines if you want to run the test immediately after uploading files
+# curl -sk "$RUN_TEST_URL" \
+#  -X POST \
+#  -H 'Content-Type: application/json' \
+#  --user "$USERNAME:$API_KEY"
+
+
+#!/bin/bash
+ 
+# Check if BRANCH is provided as a command-line argument
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 <BRANCH>"
   exit 1
 fi
  
-BRANCH="${Branch}"
+BRANCH="$1"
 TEST_ID='13758845'
-echo $Branch
  
 # GitHub repository details
 GITHUB_REPO="https://github.com/Mayankgoyal02/python-jenkins"
@@ -39,9 +87,3 @@ for file in $file_list; do
     --user "$USERNAME:$API_KEY"
   )
 done
- 
-# Uncomment the following lines if you want to run the test immediately after uploading files
-# curl -sk "$RUN_TEST_URL" \
-#  -X POST \
-#  -H 'Content-Type: application/json' \
-#  --user "$USERNAME:$API_KEY"
