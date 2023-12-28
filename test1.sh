83ae83de626560898701e3ebfd9056fdbc00db06
ethanhunt458 <59868969+ethanhunt458@users.noreply.github.com>
2023-12-28 21:43:33 +0530

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -49,10 +49,10 @@
 
 #!/bin/bash
 
-# Check if BRANCH is provided as a command-line argument or environment variable
+# Check if BRANCH is provided as a command-line argument
 if [ -z "${BRANCH}" ]; then
-  echo "Error: BRANCH parameter is not set."
-  exit 1
+ echo "Error: BRANCH parameter is not set."
+ exit 1
 fi
 
 TEST_ID='13758845'
@@ -60,33 +60,25 @@ TEST_ID='13758845'
 # GitHub repository details
 GITHUB_REPO="https://github.com/Mayankgoyal02/python-jenkins"
 RAW_BASE_URL="https://raw.githubusercontent.com/Mayankgoyal02/python-jenkins/${BRANCH}/"
-
+ 
 # BlazeMeter API details
 FILES_URL="https://a.blazemeter.com/api/v4/tests/${TEST_ID}/files"
 RUN_TEST_URL="https://a.blazemeter.com/api/v4/tests/${TEST_ID}/start"
 USERNAME='ebf4a8d99d54eb292bcad9ce'
 API_KEY='8be234d747e4d099e67040d248764c3a9d747b1c6df4c5003b813b8403b5e6c6e511ae77'
-
-# Redirect the output to a log file
-# log_file="/path/to/your/log/file.log"
-# exec > >(tee -a "${log_file}") 2>&1
-
-# Debugging output
-echo "Current working directory: $(pwd)"
-echo "BRANCH: ${BRANCH}"
-
+ 
 # Fetch the list of files with .jmx and .csv extensions from GitHub
 file_list=$(curl -s "${GITHUB_REPO}/tree/${BRANCH}" | grep -Eo 'href="([^"#]+\.jmx|[^"#]+\.csv)"' | cut -d'"' -f2)
-
+ 
 # Display the list of files (optional)
 echo "Files to be uploaded:"
 echo "$file_list"
-
+ 
 # Upload files to BlazeMeter
 for file in $file_list; do
   file_url="$RAW_BASE_URL$file"
   filename=$(basename "$file")
-
+ 
   # Upload each file individually using the provided curl command structure
   upload_response=$(curl -sk "$FILES_URL" \
     -X POST \
@@ -94,10 +86,9 @@ for file in $file_list; do
     --user "$USERNAME:$API_KEY"
   )
 done
-
+ 
 # Uncomment the following lines if you want to run the test immediately after uploading files
 # curl -sk "$RUN_TEST_URL" \
 #  -X POST \
 #  -H 'Content-Type: application/json' \
 #  --user "$USERNAME:$API_KEY"
-
