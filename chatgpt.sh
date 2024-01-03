b707f1acd0068a03cb79a46ff3f5b23af46725fe
Mayankgoyal02 <33936226+Mayankgoyal02@users.noreply.github.com>
2024-01-03 17:12:54 +0530

    Update chatgpt.sh

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,66 +1,19 @@
-$ ./get-pf.sh
-*   Trying 142.136.245.35:443...
-* Connected to bitbucket.corp.chartercom.com (142.136.245.35) port 443 (#0)
-* ALPN: offers h2
-* ALPN: offers http/1.1
-} [5 bytes data]
-* [CONN-0-0][CF-SSL] TLSv1.3 (OUT), TLS handshake, Client hello (1):
-} [512 bytes data]
-* [CONN-0-0][CF-SSL] TLSv1.3 (IN), TLS handshake, Server hello (2):
-{ [91 bytes data]
-* [CONN-0-0][CF-SSL] TLSv1.2 (IN), TLS handshake, Certificate (11):
-{ [6221 bytes data]
-* [CONN-0-0][CF-SSL] TLSv1.2 (IN), TLS handshake, Server key exchange (12):
-{ [333 bytes data]
-* [CONN-0-0][CF-SSL] TLSv1.2 (IN), TLS handshake, Server finished (14):
-{ [4 bytes data]
-* [CONN-0-0][CF-SSL] TLSv1.2 (OUT), TLS handshake, Client key exchange (16):
-} [70 bytes data]
-* [CONN-0-0][CF-SSL] TLSv1.2 (OUT), TLS change cipher, Change cipher spec (1):
-} [1 bytes data]
-* [CONN-0-0][CF-SSL] TLSv1.2 (OUT), TLS handshake, Finished (20):
-} [16 bytes data]
-* [CONN-0-0][CF-SSL] TLSv1.2 (IN), TLS handshake, Finished (20):
-{ [16 bytes data]
-* SSL connection using TLSv1.2 / ECDHE-RSA-AES128-GCM-SHA256
-* ALPN: server did not agree on a protocol. Uses default.
-* Server certificate:
-*  subject: C=US; ST=Missouri; L=St. Louis; O=Charter Communications Operating, LLC; OU=IT Architecture; CN=bitbucket.corp.chartercom.com
-*  start date: Feb  2 18:59:16 2023 GMT
-*  expire date: Feb  1 18:59:16 2025 GMT
-*  issuer: DC=com; DC=chartercom; DC=corp; CN=Charter Communications Issuing CA1
-*  SSL certificate verify result: self signed certificate in certificate chain (19), continuing anyway.
-} [5 bytes data]
-> GET /scm/smt/mobile-it-devops-cicd.git/raw/development/performance_test/ HTTP/1.1
-> Host: bitbucket.corp.chartercom.com
-> User-Agent: curl/7.87.0
-> Accept: */*
-> Authorization: Bearer NTI3OTkzNTYzOTQxOjz3y4JI9MERpV0OPebp3isLwxg2
->
-{ [5 bytes data]
-* Mark bundle as not supporting multiuse
-< HTTP/1.1 501
-< Date: Wed, 03 Jan 2024 11:32:12 GMT
-< Server: Apache/2.4.6 (Red Hat Enterprise Linux) OpenSSL/1.0.2k-fips
-< X-AREQUESTID: *3ZQ4P8x692x3876288x3
-< X-AUSERID: 75026
-< X-AUSERNAME: P3214465
-< X-ASESSIONID: jwvu57
-< Cache-Control: private, no-cache
-< Pragma: no-cache
-< x-xss-protection: 1; mode=block
-< x-frame-options: SAMEORIGIN
-< x-content-type-options: nosniff
-< Set-Cookie: BITBUCKETSESSIONID=D041A87159A2DE6F1BEC891738D04F82; Max-Age=1209600; Expires=Wed, 17 Jan 2024 11:32:13 GMT; Path=/; Secure; HttpOnly
-< Connection: close
-< Transfer-Encoding: chunked
-<
-{ [5 bytes data]
-* Closing connection 0
-} [5 bytes data]
-* [CONN-0-0][CF-SSL] TLSv1.2 (OUT), TLS alert, close notify (256):
-} [2 bytes data]
-Files to be uploaded:
+#!/bin/bash
+
+# Bitbucket repository details
+BITBUCKET_REPO='https://bitbucket.corp.chartercom.com/scm/smt/mobile-it-devops-cicd.git'  # Replace with your Bitbucket repository URL
+RAW_BASE_URL="${BITBUCKET_REPO}/raw/development/performance_test/"
+
+# Bitbucket access token
+BITBUCKET_ACCESS_TOKEN='NTI3OTkzNTYzOTQxOjz3y4JI9MERpV0OPebp3isLwxg2'
+
+# Fetch the list of files with .jmx and .csv extensions from Bitbucket
+file_list=$(curl -v -sk --header "Authorization: Bearer $BITBUCKET_ACCESS_TOKEN" "$RAW_BASE_URL" | grep -Eo 'href="([^"#]+\.jmx|[^"#]+\.csv)"' | cut -d'"' -f2)
+
+# Display the list of files
+echo "Files in the repository:"
+echo "$file_list"
+
 
 
 #!/bin/bash
